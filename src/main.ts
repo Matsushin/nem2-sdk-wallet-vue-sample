817a0c37a0d4f6660a88a2ce14deb69726e66a4d
matsushin11 <matsu1983@gmail.com>
2018-10-02 22:57:30 +0900

    nem-sdkでウォレットが使えるように

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuetify from 'vuetify'
 import store from './store';
-import './registerServiceWorker';
+import 'vue2-toast/lib/toast.css';
+import Toast from 'vue2-toast';
+import VueQriously from 'vue-qriously';
+import VueQrcodeReader from 'vue-qrcode-reader';
+
+Vue.use(Vuetify);
+Vue.use(Toast, {
+  defaultType: 'bottom',
+  duration: 3000,
+  wordWrap: true,
+  width: '280px'
+})
+Vue.use(VueQriously)
+Vue.use(VueQrcodeReader)
 
 Vue.config.productionTip = false;
 
