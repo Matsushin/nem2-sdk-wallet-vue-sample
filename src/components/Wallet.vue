ad3c2f4f1578ec752fe402bd35487648606ac372
matsushin11 <matsu1983@gmail.com>
2018-10-06 15:06:40 +0900

    XEMの送金ができるように

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,11 +1,58 @@
 <template>
   <div class="wallet">
     <div v-if="address != ''">
-        <p>アドレス：{{address}}</p>
-        <p>残高：{{balance}} xem</p>
+        <v-card-text>アドレス：{{address}}</v-card-text>
+        <v-card-text>残高：{{ balance }} xem</v-card-text>
+        <div v-for="(item, index) in validation" :key="index" class="errorLabel">
+            <div v-if="item!==true">{{ item }}</div>
+        </div>
+        <v-card-title><b>送金</b></v-card-title>
+            <v-text-field
+                label="送金先"
+                v-model="toAddress"
+                :counter="40"
+                required
+                placeholder="例. NBHWRG6STRXL2FGLEEB2UOUCBAQ27OSGDTO44UFC"
+            ></v-text-field>
+            <v-text-field
+                label="XEM"
+                v-model="toAmount"
+                type="number"
+                required
+            ></v-text-field>
+        <v-text-field
+            label="メッセージ"
+            v-model="toMessage"
+            :counter="1024"
+            placeholder="例. ありがとう"
+        ></v-text-field>
+        <v-flex>
+            <v-btn color="blue" class="white--text" @click="tapSend()">XEMを送金する</v-btn>
+        </v-flex>
+        <h2 class="mt-5">送金結果</h2>
+        <div v-if="successMessage != ''">
+            <v-alert
+            :value="true"
+            type="success"
+            >
+            成功：{{successMessage}}
+            </v-alert>
+        </div>
+        <div v-if="errorMessage != ''">
+            <v-alert
+            :value="true"
+            type="error"
+            >
+            失敗：{{errorMessage}}
+            </v-alert>
+        </div>
     </div>
     <div v-else>
-        <p>ウォレット情報取得中...</p>
+        <v-progress-circular
+            indeterminate
+            color="primary"
+        ></v-progress-circular>
+        <p>ウォレット情報を作成または取得中...</p>
     </div>
   </div>
 </template>
@@ -13,18 +60,68 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { mapGetters } from 'vuex';
-import localForage from 'localforage'
 
 @Component({
   data: () => ({
+    rules: {
+      senderAddrLimit: (value:string) => (value && (value.length === 46 || value.length === 40)) || '送金先アドレス(-除く)は40文字です。',
+      senderAddrInput: (value:string) => {
+        const pattern = /^[a-zA-Z0-9-]+$/
+        return pattern.test(value) || '送金先の入力が不正です'
+      },
+      amountLimit: (value:number) => (value >= 0) || '数量を入力してください',
+      amountInput: (value:string) => {
+        const pattern = /^[0-9.]+$/ //※ブログ上ではちゃんと表示されないため、実装の際はこのコメントアウトを外してください
+        return (pattern.test(value) && !isNaN(Number(value))) || '数量の入力が不正です'
+      },
+      messageRules: (value:string) => (value.length <= 1024) || 'メッセージの最大文字数が超えています。'
+    }
   }),
   computed: {
-      ...mapGetters({ address: 'wallet/address', balance: 'wallet/balance' }),
+      ...mapGetters({ address: 'wallet/address', balance: 'wallet/balance', successMessage: 'wallet/message', errorMessage: 'wallet/error' }),
   }
 })
 export default class Wallet extends Vue {
+    toAmount:number = 0;
+    toAddress:string = '';
+    toMessage:string = '';
+    validation:Array<any> = [];
+
     created() {
         this.$store.dispatch('wallet/loadWallet');
     }
+
+    tapSend() {
+        if (this.isValidation() === true) {
+            const payload = {
+                toAddress: this.toAddress,
+                toAmount: this.toAmount,
+                toMessage: this.toMessage,
+            };
+            this.$store.dispatch('wallet/sendXem', payload);
+        }
+    }
+
+    isValidation() {
+        this.validation = [];
+        this.validation.push(this.$data.rules.senderAddrLimit(this.toAddress));
+        this.validation.push(this.$data.rules.senderAddrInput(this.toAddress));
+        this.validation.push(this.$data.rules.amountLimit(this.toAmount));
+        this.validation.push(this.$data.rules.amountInput(this.toAmount));
+        this.validation.push(this.$data.rules.messageRules(this.toMessage));
+        let isError:Boolean = false;
+        this.validation.forEach((obj:any) => {
+        if (obj !== true) { isError = true }
+        })
+        return !isError;
+    }
 }
 </script>
+<style scoped>
+.wallet {
+  word-break: break-all;
+}
+.errorLabel {
+  color: red;
+}
+</style>
--- src/store/wallet.ts
+++ src/store/wallet.ts
@@ -1,12 +1,18 @@
-import { AccountHttp, MosaicHttp, NamespaceHttp, MosaicService, Address, SimpleWallet, Password, NetworkType } from 'nem2-sdk';
+import { AccountHttp, MosaicHttp, NamespaceHttp, MosaicService, Address, XEM, PlainMessage, TransactionHttp,
+        SimpleWallet, Password, NetworkType, Account, TransferTransaction, Deadline } from 'nem2-sdk';
 import { filter, mergeMap } from 'rxjs/operators';
 import localForage from 'localforage';
+
+const API_URL = 'http://catapult-test.44uk.net:3000/';
 
 export default {
     namespaced: true,
     state: {
         address: '',
+        privateKey: '',
         balance: 0,
+        message: '',
+        error: '',
     },
     getters: {
         address(state: any): string {
@@ -15,13 +21,28 @@ export default {
         balance(state: any): number {
             return state.balance;
         },
+        message(state: any): string {
+            return state.message;
+        },
+        error(state: any): string {
+            return state.error;
+        },
     },
     mutations: {
-        setAddress(state: any, address: any) {
+        setAddress(state: any, address: string) {
             state.address = address;
+        },
+        setPrivateKey(state: any, privateKey: string) {
+            state.privateKey = privateKey;
         },
         setBalance(state: any, balance: number) {
             state.balance = balance;
+        },
+        setMessage(state: any, message: any) {
+            state.message = message;
+        },
+        setError(state: any, error: any) {
+            state.error = error;
         },
     },
     actions: {
@@ -30,7 +51,7 @@ export default {
             let result:any = await localForage.getItem(key);
             if (result !== null) {
                 commit('setAddress', result.address);
-                const API_URL = 'http://catapult-test.44uk.net:3000';
+                commit('setPrivateKey', result.privateKey);
                 const accountHttp = new AccountHttp(API_URL);
                 const mosaicHttp = new MosaicHttp(API_URL);
                 const namespaceHttp = new NamespaceHttp(API_URL);
@@ -38,20 +59,41 @@ export default {
                 mosaicService.mosaicsAmountViewFromAddress(Address.createFromRawAddress(result.address)).pipe(
                     mergeMap(_ => _),
                     filter(mo => mo.fullName() === 'nem:xem')
-                ).subscribe(data => {
-                    commit('setBalance', data.amount.lower / 10**6);
+                ).subscribe((data) => {
+                    commit('setBalance', data.amount.lower / 10 ** 6);
                 });
             } else {
-                // ここでウォレットを作成する
-                const wallet = SimpleWallet.create('wallet', new Password('password'), NetworkType.MIJIN_TEST);
+                const password = new Password('password');
+                const wallet = SimpleWallet.create('wallet', password, NetworkType.MIJIN_TEST);
                 const walletInfo = {
                     address: wallet.address.address,
-                    privateKey: wallet.encryptedPrivateKey.encryptedKey,
-                }
+                    privateKey: wallet.encryptedPrivateKey.decrypt(password),
+                };
                 let key = 'wallet';
-                let result:any = await localForage.setItem(key, walletInfo);
+                await localForage.setItem(key, walletInfo);
                 commit('setAddress', walletInfo.address);
+                commit('setPrivateKey', walletInfo.privateKey);
             }
-        }
+        },
+        sendXem({ commit }, payload: any) {
+            const transferTransaction = TransferTransaction.create(
+                Deadline.create(),
+                Address.createFromRawAddress(payload.toAddress),
+                [XEM.createRelative(parseInt(payload.toAmount, 10))],
+                PlainMessage.create(payload.toMessage),
+                NetworkType.MIJIN_TEST,
+            );
+            const account = Account.createFromPrivateKey(this.state.wallet.privateKey, NetworkType.MIJIN_TEST);
+            const signedTransaction = account.sign(transferTransaction);
+            const transactionHttp = new TransactionHttp(API_URL);
+            transactionHttp.announce(signedTransaction).subscribe(
+                res => {
+                    commit('setMessage', res.message);
+                },
+                err => {
+                    commit('setError', err.toString());
+                }
+            );
+        },
     },
 }
